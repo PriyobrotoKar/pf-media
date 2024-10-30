cffa5fcc668a5aa38184ff7eda1e3105ffb1cdd2
Priyobroto Kar <priyobrotokar@gmail.com>
2024-10-30 23:37:46 +0530

    Completed the herosection

---
+++ src/components/Header.tsx
@@ -0,0 +1,60 @@
+import FeatherIcon from "feather-icons-react";
+import React from "react";
+import { Button, buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
+
+const navLinks = [
+  {
+    title: "Home",
+    url: "/",
+  },
+  {
+    title: "Services",
+    url: "/services",
+  },
+  {
+    title: "Referal Program",
+    url: "/referal",
+  },
+  {
+    title: "Products",
+    url: "/products",
+  },
+];
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center px-8 py-5">
+      <div className="text-[1.125rem] lg:text-lg text-primary font-medium">
+        PF Media.
+      </div>
+      <nav className="bg-secondary p-2 rounded-full space-x-1 hidden lg:block">
+        {navLinks.map((link) => (
+          <a
+            key={link.url}
+            href={link.url}
+            className={cn(
+              buttonVariants({
+                variant: link.title === "Home" ? "default" : "ghost",
+                size: "sm",
+                className: "lg:h-9",
+              })
+            )}
+          >
+            {link.title}
+          </a>
+        ))}
+      </nav>
+      <Button
+        variant={"secondary"}
+        size={"sm"}
+        className="hidden lg:inline-flex"
+      >
+        Get a quote <FeatherIcon icon="arrow-up-right" />
+      </Button>
+      <FeatherIcon icon="menu" className="text-primary lg:hidden" />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/sections/Herosection.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/ui/button";
+import FeatherIcon from "feather-icons-react";
+import Image from "next/image";
+import React from "react";
+
+const Herosection = () => {
+  return (
+    <div className="text-center md:text-left max-w-4xl lg:max-w-[1440px] mx-auto flex justify-center items-center lg:items-start gap-14 md:gap-24 px-14 pt-8 lg:pt-16 xl:pt-32">
+      <div className="max-w-xs md:max-w-none lg:py-7 flex-1">
+        <h1 className="text-lg md:text-xl lg:text-2xl font-medium mb-5 md:mb-4 lg:mb-8 mx-auto ">
+          Boost & Scale Your Online Presence at 60% off Today!
+        </h1>
+        <p className="text-sm lg:text-lg max-w-xl  font-light mb-9 md:mb-7 lg:mb-12">
+          Amplify engagement with targeted ads, social media, and a custom
+          website.
+        </p>
+        <Button>
+          Claim Offer <FeatherIcon icon="arrow-up-right" size={45} />
+        </Button>
+      </div>
+      <div className="hidden md:block w-[40%]">
+        <Image
+          src={"/heroimage.svg"}
+          alt="Hero image"
+          className="w-full xl:w-[30rem]"
+          width={300}
+          height={300}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Herosection;
